5a85ab27d05897f69ea5857e7b2d3cc7eee2412f
yun8565 <yunyun8565@naver.com>
2022-07-13 00:12:31 +0900

    build: 스프링부트 프로젝트 시작 - #7
    프로젝트 초기화
    초기 dependencies:  Spring web, Actuator, DevTools, Lombok

---
+++ src/test/java/boardspringboot/BoardSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package boardspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
