f407bf1f3ec79348ded284e016fa3df1bbf556ec
yun8565 <yunyun8565@naver.com>
2022-08-02 01:30:43 +0900

    feat: 페이지네이션 기능을 컨트롤러에 반영 - #25
    테스트는 Page 클래스를 이용하기 때문에 
    페이징과 정렬을 함께 테스트해봄

--- src/main/java/boardspringboot/controller/ArticleController.java
+++ src/main/java/boardspringboot/controller/ArticleController.java
@@ -4,7 +4,9 @@ import boardspringboot.domain.type.SearchType;
 import boardspringboot.dto.response.ArticleResponse;
 import boardspringboot.dto.response.ArticleWithCommentsResponse;
 import boardspringboot.service.ArticleService;
+import boardspringboot.service.PaginationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
@@ -15,12 +17,15 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RequestMapping("/articles")
 @Controller
 public class ArticleController {
 
     private final ArticleService articleService;
+    private final PaginationService paginationService;
 
     @GetMapping
     public String articles(
@@ -29,7 +34,12 @@ public class ArticleController {
             @PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
             ModelMap map
     ) {
-        map.addAttribute("articles", articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from));
+        Page<ArticleResponse> articles = articleService.searchArticles(searchType, searchValue, pageable).map(ArticleResponse::from);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(), articles.getTotalPages());
+
+        map.addAttribute("articles", articles);
+        map.addAttribute("paginationBarNumbers", barNumbers);
+
         return "articles/index";
     }
 
